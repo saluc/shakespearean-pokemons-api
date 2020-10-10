2ca6cb2f89d1867a33a3b9fdb9df7aaf45e972b5
Luca Salucci <salucci68@gmail.com>
2020-10-10 19:38:44 +0200

    Updated dependencies and added caching

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,29 +1,45 @@
 const Pokedex = require("../modules/pokedex.js");
 const ShakeTransl = require("../modules/shakespeareanTranslator.js");
+const cache = require('memory-cache');
+
+const cacheDuration = 600;
 const p = new Pokedex();
 const st = new ShakeTransl();
+const memCache = new cache.Cache();
 
 function getPokemonDescription(req, res) {
-    return p.getPokemonDescriptionByName(req.params && req.params.name)
-        .then((resp) => {
-            console.log(`Pokemon with name '${req.params.name}' found! Its description is: '${resp}'`);
-            return resp;
-        })
-        .then((desc) => {
-            return st.translate(desc).then((translDesc) => {
-                console.log(`Pokemon description translated to Shakespearean! The translation is: '${translDesc}'`);
-                return ({ status: 200, json: { name: req.params.name, description: translDesc } });
-            });
-        })
-        .catch((err) => {
-            console.error(`Error: ${err.message}`);
-            if (err.message == "Request failed with status code 404") {
-                return ({ status: 404, json: { error: err.message } });
-            }
-            else {
-                return ({ status: 500, json: { error: err.message } });
-            }
+    let cacheContent = memCache.get(req.params.name);
+    if (cacheContent) {
+        console.log(`Cache hit, found cached description for ${req.params.name}`);
+        return Promise.resolve().then(() => {
+            return { status: 200, json: { name: req.params.name, description: cacheContent } }
         });
+    }
+    else {
+        console.log(`Cache miss, requesting description for ${req.params.name}`);       
+        return p.getPokemonDescriptionByName(req.params && req.params.name)
+            .then((resp) => {
+                console.log(`Pokemon with name '${req.params.name}' found! Its description is: '${resp}'`);
+                return resp;
+            })
+            .then((desc) => {
+                return st.translate(desc).then((translDesc) => {
+                    console.log(`Pokemon description translated to Shakespearean! The translation is: '${translDesc}'`);
+                    memCache.put(req.params.name, translDesc, cacheDuration * 1000);
+                    return ({ status: 200, json: { name: req.params.name, description: translDesc } });
+                });
+            })
+            .catch((err) => {
+                console.error(`Error: ${err.message}`);
+                if (err.message == "Request failed with status code 404") {
+                    return ({ status: 404, json: { error: err.message } });
+                }
+                else {
+                    return ({ status: 500, json: { error: err.message } });
+                }
+            });
+            
+    }
 }
 
 module.exports = { getPokemonDescription };

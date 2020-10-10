430052a6d633dcd0a583fad661132b8b2fcfd97f
Luca Salucci <salucci68@gmail.com>
2020-10-10 16:25:38 +0200

    Created pokedex.js module to get pokèmon descriptions

---
+++ src/modules/pokedex.js
@@ -0,0 +1,26 @@
+var _Pokedex = require('pokedex-promise-v2');
+
+class Pokedex {
+
+    constructor(language = "en", version = "red") {
+        this.pokedex = new _Pokedex();
+        this.language = language;
+        this.version = version;
+    }
+
+    async getPokemonDescriptionByName(name) {
+        let lan = this.language;
+        let ver = this.version;
+        return this.pokedex.getPokemonSpeciesByName(name)
+            .then(function (response) {
+                let desc = response && response.flavor_text_entries.find(el => el.language.name == lan && el.version.name == ver);
+                return desc && desc.flavor_text.replace(/(\r\n|\n|\f)/gm, " ");
+            })
+            .catch(function (error) {
+                console.error(`Pokedex error occurred: ${error}`);
+                throw(error);
+            });
+    }
+}
+
+module.exports = Pokedex;
